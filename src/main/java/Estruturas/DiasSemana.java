083b9bbac22db943d43cfc8de375dda2239f9186
marceloairesdasneves <141940013+marceloairesdasneves@users.noreply.github.com>
2026-05-12 18:19:30 -0300

    Java algaWorks, estrutura de repeticao.

--- src/main/java/Estruturas/DiasSemana.java
+++ src/main/java/Estruturas/DiasSemana.java
@@ -2,8 +2,39 @@ package Estruturas;
 
 // @author Marcelo Neves
 
+import java.util.Scanner;
+
 public class DiasSemana {
-    static void main(String[] args) {
-        System.out.println("Dias da semana 4:");
+    static void main() {
+        try(Scanner input = new Scanner(System.in)) {
+            System.out.print("Digite um numero da Semana (1 - 7): ");
+            int numero = input.nextInt();
+
+            switch (numero) {
+                case 1:
+                    System.out.println("Domingo");
+                    break;
+                case 2:
+                    System.out.println("Segunda-feira");
+                    break;
+                case 3:
+                    System.out.println("Terça-feira");
+                    break;
+                case 4:
+                    System.out.println("Quarta-feira");
+                    break;
+                case 5:
+                    System.out.println("Quinta-feira");
+                    break;
+                case 6:
+                    System.out.println("Sexta-feira");
+                    break;
+                case 7:
+                    System.out.println("Sabado");
+                    break;
+                default:
+                    System.out.println("Numero Invalido");
+            }
+        }
     }
 }
