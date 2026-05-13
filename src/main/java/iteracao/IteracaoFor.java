346b84e1f5c8a7473f78e688a94dcd5f319466e4
marceloairesdasneves <141940013+marceloairesdasneves@users.noreply.github.com>
2026-05-12 21:44:48 -0300

    SDK 21 is not compatible

--- src/main/java/iteracao/IteracaoFor.java
+++ src/main/java/iteracao/IteracaoFor.java
@@ -3,7 +3,7 @@ package iteracao;
 // @author Marcelo Neves
 
 public class IteracaoFor {
-    static void main() {
+    public static void main(String[] args) {
         int[] numeros = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
         for (int numero : numeros) {
             if (numero % 3 == 0) {
