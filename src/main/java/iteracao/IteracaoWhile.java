7a3e7902b2b3393faf97d49be8d39f266e92108f
marceloairesdasneves <141940013+marceloairesdasneves@users.noreply.github.com>
2026-05-12 21:37:36 -0300

    checando o sonnar para verificar as mensagens

--- src/main/java/iteracao/IteracaoWhile.java
+++ src/main/java/iteracao/IteracaoWhile.java
@@ -3,7 +3,7 @@ package iteracao;
 // @author Marcelo Neves
 
 public class IteracaoWhile {
-    static void main() {
+    static void main(String[] args) {
         int[] numeros = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
         int i = 0;
         while (i < numeros.length) {
