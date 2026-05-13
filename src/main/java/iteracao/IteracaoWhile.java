87de36a9aee5ff1f4e80abee14c19bbafe780a83
marceloairesdasneves <141940013+marceloairesdasneves@users.noreply.github.com>
2026-05-12 21:35:33 -0300

    Aplicando a regra para iteracaoWhile exemplo algoWors

---
+++ src/main/java/iteracao/IteracaoWhile.java
@@ -0,0 +1,16 @@
+package iteracao;
+
+// @author Marcelo Neves
+
+public class IteracaoWhile {
+    static void main() {
+        int[] numeros = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        int i = 0;
+        while (i < numeros.length) {
+            if (numeros[i] % 3 == 0) {
+                System.out.println("Estes números divisíveis por 3: " + numeros[i]);
+            }
+            i++;
+        }
+    }
+}
